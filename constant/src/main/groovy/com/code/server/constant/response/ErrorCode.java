c3ac13a848afa7553200021bae991c312937cca9
TJjifang <546507232@qq.com>
2017-06-05 15:17:01 +0800

    添加一个错误编号

--- constant/src/main/groovy/com/code/server/constant/response/ErrorCode.java
+++ constant/src/main/groovy/com/code/server/constant/response/ErrorCode.java
@@ -23,6 +23,7 @@ public class ErrorCode {
 	public static final int CANNOT_JOIN_ROOM_IS_IN_GAME = 1000014;
 	public static final int CANNOT_QUIT_ROOM_IS_IN_GAME = 1000015;
 	public static final int CANNOT_CREATE_ROOM_MONEY = 1000016;
+	public static final int REDIS_NO_TOKEN = 1000017;
 
 	//userId获取不到Role
 	public static final int USERID_ERROR = 110001;
