14e797d0ad4f49bf4b1214ec44f50c65af222823
TJjifang <546507232@qq.com>
2017-06-08 16:43:55 +0800

    修改redis

--- login/src/main/java/com/code/server/login/action/LoginAction.java
+++ login/src/main/java/com/code/server/login/action/LoginAction.java
@@ -28,6 +28,8 @@ import java.io.UnsupportedEncodingException;
 import java.net.URLDecoder;
 import java.util.*;
 
+import static com.sun.org.apache.xalan.internal.xsltc.compiler.util.Type.Text;
+
 
 /**
  * Created by win7 on 2017/3/8.
@@ -56,11 +58,14 @@ public class LoginAction {
         return constantService.constantDao.findOne(1L);
     }
 
+    private String ip ="192.168.1.150";
+    private String port = "8080";
+
     @RequestMapping("/login")
     public Map<String,Object> login( String account,String password,String token_user){
-             List<Object> params = new ArrayList<>();
+             Map<String,Object> params = new HashMap<>();
              int code = 0;
-           Long userid = userRedisService.getUserIdByAccount(account);//玩家id
+           String userid = userRedisService.getUserIdByAccount(account);//玩家id
 
         if(userid==null){
             User user = userService.getUserByAccountAndPassword(account, password);
@@ -81,24 +86,24 @@ public class LoginAction {
                 userBean.setMarquee(getConstant().getMarquee());
                 userBean.setDownload2(getConstant().getDownload2());
 
-                userRedisService.setUserBean(userBean);//userid-userbean
-                userRedisService.setUserMoney(user.getUserId(), user.getMoney());//userid-money
+               // userRedisService.setUserBean(userBean);//userid-userbean
+               // userRedisService.setUserMoney(user.getUserId(), user.getMoney());//userid-money
 
 
                 long time = System.currentTimeMillis();
 
                 String token = MD5Util.MD5Encode(time + account + password, "UTF-8");
 
-                userRedisService.setToken(user.getUserId(),token);//userid-token
+                //userRedisService.setToken(user.getUserId(),token);//userid-token
 
-                userRedisService.setAccountUserId(account,user.getUserId());//account-userid
-                userRedisService.setUserIdAccount(user.getUserId(),account);//userid-account
+                //userRedisService.setAccountUserId(account,user.getUserId()+"");//account-userid
+                ///userRedisService.setUserIdAccount(user.getUserId()+"",account);//userid-account
 
-                userRedisService.setOpenIdUserId(user.getOpenId(),user.getUserId());//openid-userid
-                userRedisService.setUserIdOpenId(user.getUserId(),user.getOpenId());//userid-openid
+               // userRedisService.setOpenIdUserId(user.getOpenId(),user.getUserId()+"");//openid-userid
+                //userRedisService.setUserIdOpenId(user.getUserId()+"",user.getOpenId());//userid-openid
 
-                params.add(token);
-                params.add(user.getUserId());
+                params.put("token",token);
+                params.put("userId",user.getUserId());
             } else {//密码错误
                 if (serverService.getAllServerInfo().get(0).getAppleCheck() == 1) {
                     user = createUser(account, password);
@@ -119,43 +124,46 @@ public class LoginAction {
                     userBean.setMarquee(getConstant().getMarquee());
                     userBean.setDownload2(getConstant().getDownload2());
 
-                    userRedisService.setUserBean(userBean);//userid-userbean
-                    userRedisService.setUserMoney(user.getUserId(), user.getMoney());//userid-money
+                    //userRedisService.setUserBean(userBean);//userid-userbean
+                    //userRedisService.setUserMoney(user.getUserId(), user.getMoney());//userid-money
 
                     long time = System.currentTimeMillis();
 
                     String token = MD5Util.MD5Encode(time + account + password, "UTF-8");
 
-                    userRedisService.setToken(user.getUserId(),token);//userid-token
+                    //userRedisService.setToken(user.getUserId(),token);//userid-token
 
-                    userRedisService.setAccountUserId(account,user.getUserId());//account-userid
-                    userRedisService.setUserIdAccount(user.getUserId(),account);//userid-account
+                    //userRedisService.setAccountUserId(account,user.getUserId()+"");//account-userid
+                    //userRedisService.setUserIdAccount(user.getUserId()+"",account);//userid-account
 
-                    userRedisService.setOpenIdUserId(user.getOpenId(),user.getUserId());//openid-userid
-                    userRedisService.setUserIdOpenId(user.getUserId(),user.getOpenId());//userid-openid
+                    //userRedisService.setOpenIdUserId(user.getOpenId(),user.getUserId()+"");//openid-userid
+                    //userRedisService.setUserIdOpenId(user.getUserId()+"",user.getOpenId());//userid-openid
 
-                    params.add(getUserVo(user));
-                    params.add(token);
-                    params.add(user.getUserId());
+
+                    params.put("user",getUserVo(user));
+                    params.put("token",token);
+                    params.put("userId",user.getUserId());
                 } else {
                     code = ErrorCode.ROLE_ACCOUNT_OR_PASSWORD_ERROR;
                 }
             }
         }else{
-            String redisTokey =userRedisService.getToken(userid);
+            //String redisTokey = userRedisService.getToken(Long.valueOf(userid));
+
+            String redisTokey = null;
             //判断token是否存在redis
             if(token_user!=null && redisTokey!=null){
                 if(token_user.equals(redisTokey)){
-                    params.add(token_user);
+                    params.put("token",token_user);
                 }else{
                     code = ErrorCode.REDIS_NO_TOKEN;
                 }
             }else {
-                UserBean userBeanRedis = userRedisService.getUserBean(userid);
+                UserBean userBeanRedis = userRedisService.getUserBean(Long.valueOf(userid));
                 //判断redis是否有UserBean
                 if(userBeanRedis!=null){
                     if(password.equals(userBeanRedis.getPassword())){
-                        params.add(userBeanRedis);
+                        params.put("user",userBeanRedis);
                     }else{
                         code = ErrorCode.ROLE_ACCOUNT_OR_PASSWORD_ERROR;
                     }
@@ -165,45 +173,57 @@ public class LoginAction {
             }
         }
 
+        params.put("ip",ip);
+        params.put("port",port);
         return getParams("login",params,code);
     }
 
 
-    @RequestMapping("/checkOpenId")
-    public Map<String,Object> checkOpenId( final String openId,String username,final String image,int sex,String token_user){
-        int code = 0;
-        User user = new User();
-        List<Object> params = new ArrayList<>();
 
-        Long userid = userRedisService.getUserIdByOpenId(openId);
+    @RequestMapping("/checkOpenId")
+    public Map<String,Object> checkOpenId(String openId,String username,String image,int sex,String token_user){
+        System.out.println("openId:"+openId);
+        System.out.println("username:"+username);
+        System.out.println("image:"+image);
+        System.out.println("sex:"+sex);
+        System.out.println("token_user:"+token_user);
+
+
+        int code = 0;
+        User newuser = new User();
+        Map<String,Object> params = new HashMap<>();
+
+        String userid = userRedisService.getUserIdByOpenId(openId);
 
         if(userid==null){
-            user = userService.getUserByOpenId(openId);
+           User user = userService.getUserByOpenId(openId);
 
             String img = image;
             if(img == null || img.equals("")){
                 img = "https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=253777390,947512827&fm=23&gp=0.jpg/96";
             }
             if(user == null) {
-                user = new User();
+
 //                user.setId(0);
-//                user.setUserId(GameManager.getInstance().nextId());
-                user.setOpenId(openId);
-                user.setAccount(UUID.randomUUID().toString());
-                user.setPassword("111111");
-                try {
-                    user.setUsername(URLDecoder.decode(username, "utf-8"));
+                //user.setUserId(new IdWorker(serverId,1).nextId());
+                newuser.setOpenId(openId);
+                newuser.setAccount(UUID.randomUUID().toString());
+                newuser.setPassword("111111");
+                /*try {
+                    newuser.setUsername(URLDecoder.decode(username, "utf-8"));
                 } catch (UnsupportedEncodingException e) {
                     e.printStackTrace();
-                }
-                user.setImage(img);
-                user.setSex(sex);
-                user.setVip(0);
-                user.setUuid("0");
-                user.setMoney(getConstant().getInitMoney());
-                userService.save(user);
+                }*/
 
-                params.add(getUserVo(user));
+                //newuser.setUsername(username);
+                newuser.setImage(img);
+                newuser.setSex(sex);
+                newuser.setVip(0);
+                newuser.setUuid("0");
+                newuser.setMoney(getConstant().getInitMoney());
+                newuser = userService.save(newuser);
+
+                params.put("user",getUserVo(newuser));
             }else{
                 try {
                     user.setUsername(URLDecoder.decode(username, "utf-8"));
@@ -214,7 +234,7 @@ public class LoginAction {
                 user.setSex(sex);
                 userService.save(user);
 
-                params.add(getUserVo(user));
+                params.put("user",getUserVo(user));
             }
 
 
@@ -233,34 +253,39 @@ public class LoginAction {
             userBean.setMarquee(getConstant().getMarquee());
             userBean.setDownload2(getConstant().getDownload2());
 
-            userRedisService.setUserBean(userBean);//userid-userbean
-            userRedisService.setUserMoney(user.getUserId(), user.getMoney());//userid-money
+            //userRedisService.setUserBean(userBean);//userid-userbean
+            //userRedisService.setUserMoney(user.getUserId(), user.getMoney());//userid-money
 
 
             long time = System.currentTimeMillis();
 
             String token = MD5Util.MD5Encode(time + user.getAccount() + user.getPassword(), "UTF-8");
 
-            userRedisService.setToken(user.getUserId(),token);//userid-token
+            //userRedisService.setToken(user.getUserId(),token);//userid-token
 
-            userRedisService.setAccountUserId(user.getAccount(),user.getUserId());//account-userid
-            userRedisService.setUserIdAccount(user.getUserId(),user.getAccount());//userid-account
+            //userRedisService.setAccountUserId(user.getAccount(),user.getUserId()+"");//account-userid
+            //userRedisService.setUserIdAccount(user.getUserId()+"",user.getAccount());//userid-account
 
-            userRedisService.setOpenIdUserId(user.getOpenId(),user.getUserId());//openid-userid
-            userRedisService.setUserIdOpenId(user.getUserId(),user.getOpenId());//userid-openid
+            //userRedisService.setOpenIdUserId(user.getOpenId(),user.getUserId()+"");//openid-userid
+            //userRedisService.setUserIdOpenId(user.getUserId()+"",user.getOpenId());//userid-openid
 
-            params.add(token);
+            params.put("userid",user.getUserId());
+            params.put("token",token);
         }else{
-            String redisTokey =userRedisService.getToken(userid);
+            String redisTokey =userRedisService.getToken(Long.valueOf(userid));
             //判断token是否存在redis
             if(token_user!=null && redisTokey!=null){
                 if(token_user.equals(redisTokey)){
-                    params.add(token_user);
+                    params.put("token",token_user);
                 }else{
                     code = ErrorCode.REDIS_NO_TOKEN;
                 }
             }
         }
+
+
+        params.put("ip",ip);
+        params.put("port",port);
 
         return getParams("checkOpenId",params,code);
     }
@@ -272,11 +297,37 @@ public class LoginAction {
         params.put("address",getConstant().getDownload());
         params.put("appleVersion",serverService.getAllServerInfo().get(0).getVersionOfIos());
         params.put("androidVersion",serverService.getAllServerInfo().get(0).getVersionOfAndroid());
+        params.put("ip",ip);
+        params.put("port",port);
         return getParams("appleCheck",params,0);
     }
 
 
-    public Map<String,Object> getParams(String url , Object params,int code){
+    @RequestMapping("/appleCheck123")
+    public Map<String,Object> appleCheck123(){
+        Map<String,Object> params = new HashMap<>();
+        User user = new User();
+
+        user.setOpenId("1");
+        user.setAccount(UUID.randomUUID().toString());
+        user.setPassword("111111");
+        try {
+            user.setUsername(URLDecoder.decode("123", "utf-8"));
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+        user.setImage("https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=253777390,947512827&fm=23&gp=0.jpg/96");
+        user.setSex(1);
+        user.setVip(0);
+        user.setUuid("0");
+        user.setMoney(getConstant().getInitMoney());
+        user = userService.save(user);
+        params.put("user",user);
+        return getParams("appleCheck123",params,0);
+    }
+
+
+    public Map<String,Object> getParams(String url,Object params,int code){
         Map<String,Object> results = new HashMap<>();
         results.put("url",url);
         results.put("params",params);
@@ -337,23 +388,6 @@ public class LoginAction {
 
     }
 
-    public static void main(String[] args) {
-        Map<Integer,Integer> map = new HashMap<>();
-        List<Test> list = new ArrayList<>();
-        list.add(new Test());
-        list.add(new Test());
-        list.add(new Test());
-        list.add(new Test());
-        list.add(new Test());
-
-        map.put(1, 2);
-        map.put(2, 2);
-        map.put(3, 2);
-        map.put(4, 2);
-        map.put(5, 2);
-
-
-    }
 
 
     public static class Test{
