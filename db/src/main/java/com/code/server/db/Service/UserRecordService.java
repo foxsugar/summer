2b140683c3d8dafab1268f1f073248f2189d76f0
foxsugar <redalert3@126.com>
2017-06-19 10:19:46 +0800

    保存玩家

--- db/src/main/java/com/code/server/db/Service/UserRecordService.java
+++ db/src/main/java/com/code/server/db/Service/UserRecordService.java
@@ -1,11 +1,11 @@
 package com.code.server.db.Service;
 
+import com.code.server.constant.game.Record;
 import com.code.server.db.dao.IUserRecordDao;
-import com.code.server.db.model.Record;
-import com.code.server.db.model.User;
 import com.code.server.db.model.UserRecord;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import java.util.List;
