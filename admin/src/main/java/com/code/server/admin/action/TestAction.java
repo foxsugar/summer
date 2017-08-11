66f2aa40ab5fe1186352d2623cf16f01b7890d3d
foxsugar <redalert3@126.com>
2017-08-11 10:30:12 +0800

    admin 测试

--- admin/src/main/java/com/code/server/admin/action/TestAction.java
+++ admin/src/main/java/com/code/server/admin/action/TestAction.java
@@ -1,11 +1,14 @@
 package com.code.server.admin.action;
 
+import com.code.server.util.JsonUtil;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by sunxianping on 2017/8/11.
@@ -21,7 +24,11 @@ public class TestAction {
     public String callback(HttpServletRequest request, HttpServletResponse response) {
 
 
+        Map map = new HashMap<>();
+        map.put("a", "a");
+        map.put("b", "b");
+        map.put("c", "c");
 
-        return "";
+        return JsonUtil.toJson(map);
     }
 }
