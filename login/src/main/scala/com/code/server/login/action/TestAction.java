870dd35e98ab5f6cf8c31129f7486a000135625f
zhaobin <1067715297@qq.com>
2017-12-26 10:04:50 +0800

    zbb

--- login/src/main/scala/com/code/server/login/action/TestAction.java
+++ login/src/main/scala/com/code/server/login/action/TestAction.java
@@ -1,7 +1,13 @@
 package com.code.server.login.action;
+import com.code.server.constant.game.UserBean;
+import com.code.server.constant.response.ResponseVo;
 import com.code.server.db.Service.ChargeService;
+import com.code.server.db.Service.UserService;
 import com.code.server.db.model.Charge;
+import com.code.server.db.model.User;
+import com.code.server.login.kafka.MsgSender;
 import com.code.server.login.util.PayUtil;
+import com.code.server.redis.service.UserRedisService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,6 +22,8 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by sunxianping on 2017/12/20.
@@ -25,6 +33,12 @@ public class TestAction {
 
     @Autowired
     private ChargeService chargeService;
+
+    @Autowired
+    private UserRedisService userRedisService;
+
+    @Autowired
+    private UserService userService;
 
     protected static Logger logger= LoggerFactory.getLogger(TestAction.class);
 
@@ -82,6 +96,24 @@ public class TestAction {
                 charge.setStatus(1);
                 chargeService.save(charge);
                 logger.info("支付成功！");
+
+                UserBean UserBeanRedis = userRedisService.getUserBean(charge.getUserid());
+
+                double addMoney = charge.getMoney();
+
+                if (UserBeanRedis != null) {
+                    userRedisService.addUserMoney(charge.getUserid(), addMoney);
+                } else {
+                    User user = userService.getUserByUserId(charge.getUserid());
+                    System.out.println("修改玩家豆豆");
+                    user.setMoney(user.getMoney() + addMoney);
+                    userService.save(user);
+                }
+
+                System.out.println("通知客户端刷新充值");
+                Map<String, String> rs = new HashMap<>();
+                MsgSender.sendMsg2Player(new ResponseVo("userService", "refresh", rs), charge.getUserid());
+
             }else{
                 logger.info("支付失败");
             }
@@ -130,10 +162,29 @@ public class TestAction {
         logger.info("-sign：{}", sign);
         if (sign.equals(md5sign)){
             if(returncode.equals("00")){
+
                 Charge charge = chargeService.getChargeByOrderid(orderid);
                 charge.setStatus(1);
                 chargeService.save(charge);
                 logger.info("支付成功！");
+
+                UserBean UserBeanRedis = userRedisService.getUserBean(charge.getUserid());
+
+                double addMoney = charge.getMoney();
+
+                if (UserBeanRedis != null) {
+                    userRedisService.addUserMoney(charge.getUserid(), addMoney);
+                } else {
+                    User user = userService.getUserByUserId(charge.getUserid());
+                    System.out.println("修改玩家豆豆");
+                    user.setMoney(user.getMoney() + addMoney);
+                    userService.save(user);
+                }
+
+                System.out.println("通知客户端刷新充值");
+                Map<String, String> rs = new HashMap<>();
+                MsgSender.sendMsg2Player(new ResponseVo("userService", "refresh", rs), charge.getUserid());
+
 
             }else{
                 logger.info("支付失败");
