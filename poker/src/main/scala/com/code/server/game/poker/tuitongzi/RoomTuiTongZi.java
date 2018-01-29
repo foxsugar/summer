4e872a5e0bcbb4c9ea3475138c9fc2b444ebb5da
zhaobin <1067715297@qq.com>
2018-01-29 17:07:17 +0800

    Merge branch 'master' of https://github.com/foxsugar/summer
    # Conflicts:
    #	poker/src/main/scala/com/code/server/game/poker/cow/GameCow.java
    #	poker/src/test/java/com/code/server/game/poker/cow/CowPlayerTest.java

--- poker/src/main/scala/com/code/server/game/poker/tuitongzi/RoomTuiTongZi.java
+++ poker/src/main/scala/com/code/server/game/poker/tuitongzi/RoomTuiTongZi.java
@@ -1,12 +1,18 @@
 package com.code.server.game.poker.tuitongzi;
 
+import com.code.server.constant.exception.DataNotFoundException;
+import com.code.server.constant.response.ErrorCode;
 import com.code.server.constant.response.ResponseVo;
 import com.code.server.game.poker.config.ServerConfig;
 import com.code.server.game.room.Room;
 import com.code.server.game.room.kafka.MsgSender;
 import com.code.server.game.room.service.RoomManager;
+import com.code.server.redis.config.IConstant;
+import com.code.server.redis.service.RedisManager;
 import com.code.server.util.IdWorker;
 import com.code.server.util.SpringUtil;
+import com.code.server.util.timer.GameTimer;
+import com.code.server.util.timer.TimerNode;
 
 public class RoomTuiTongZi extends Room{
 
@@ -39,7 +45,7 @@ public class RoomTuiTongZi extends Room{
         }
     }
 
-    public static int createRoom(long userId, String roomType,String gameType, int gameNumber, int personNumber, boolean isJoin, int multiple){
+    public static int createRoom(long userId, String roomType,String gameType, int gameNumber, int personNumber, boolean isJoin, int multiple) throws DataNotFoundException {
         RoomTuiTongZi room = getRoomInstance(roomType);
 
         room.personNumber = personNumber;
@@ -50,13 +56,37 @@ public class RoomTuiTongZi extends Room{
         room.multiple = multiple;
         room.bankerId = userId;
 
+        room.init(gameNumber, multiple);
+
+        int code = room.joinRoom(userId, isJoin);
+        if (code != 0) {
+            return code;
+        }
+
+
+        //代建房 定时解散
+        if (!isJoin) {
+            //给代建房 开房者 扣钱
+            if(RedisManager.getUserRedisService().getUserMoney(userId) < room.createNeedMoney){
+                return ErrorCode.CANNOT_CREATE_ROOM_MONEY;
+            }
+            room.spendMoney();
+            TimerNode prepareRoomNode = new TimerNode(System.currentTimeMillis(), IConstant.HOUR_1, false, room::dissolutionRoom);
+            room.prepareRoomTimerNode = prepareRoomNode;
+            GameTimer.addTimerNode(prepareRoomNode);
+        }
+
         ServerConfig serverConfig = SpringUtil.getBean(ServerConfig.class);
         RoomManager.addRoom(room.roomId, "" + serverConfig.getServerId(), room);
+
         IdWorker idWorker = new IdWorker(serverConfig.getServerId(), 0);
         room.setUuid(idWorker.nextId());
 
-        MsgSender.sendMsg2Player(new ResponseVo("gameTuiTongZiService", "createTuiTongZiRoom", room.toVo(userId)), userId);
+        MsgSender.sendMsg2Player(new ResponseVo("pokerRoomService", "createTTZRoom", room.toVo(userId)), userId);
 
         return 0;
     }
+
+    private void init(){}
+
 }
