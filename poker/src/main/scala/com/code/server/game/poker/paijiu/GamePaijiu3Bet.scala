872694faed68aee73e0f6eb726a7ed22e84ed7f4
clarkkent <lykaka22@126.com>
2018-03-28 15:04:39 +0800

    fix 179

--- poker/src/main/scala/com/code/server/game/poker/paijiu/GamePaijiu3Bet.scala
+++ poker/src/main/scala/com/code/server/game/poker/paijiu/GamePaijiu3Bet.scala
@@ -22,6 +22,7 @@ class GamePaijiu3Bet extends GamePaijiu{
     */
   override protected def compareAndSetScore(banker: PlayerCardInfoPaijiu, other: PlayerCardInfoPaijiu): Int = {
     val mix8Score = getGroupScoreByName(MIX_8)
+    val sky8Score = getGroupScoreByName(SKY_8)
     val bankerScore1 = getGroupScore(banker.group1)
     val bankerScore2 = getGroupScore(banker.group2)
     val otherScore1 = getGroupScore(other.group1)
@@ -33,30 +34,30 @@ class GamePaijiu3Bet extends GamePaijiu{
     if (bankerScore2 < otherScore2) result -= 1
     //庄家赢
     if (result > 0) {
-      val changeScore = other.getBetScore(bankerScore2 == mix8Score)
+      val changeScore = other.getBetScore(bankerScore2 >= mix8Score && bankerScore2 <= sky8Score)
       banker.addScore(this.roomPaijiu, changeScore)
       other.addScore(this.roomPaijiu, -changeScore)
       roomPaijiu.addUserSocre(banker.userId, changeScore)
       roomPaijiu.addUserSocre(other.userId, -changeScore)
-      if(bankerScore2 > mix8Score) {//第三道
-        banker.addScore(this.roomPaijiu, banker.bet.three)
-        other.addScore(this.roomPaijiu, -banker.bet.three)
-        roomPaijiu.addUserSocre(banker.userId, banker.bet.three)
-        roomPaijiu.addUserSocre(other.userId, -banker.bet.three)
-      }
-      other.winState = LOSE
-    } else if (result < 0) {
-      //闲家赢
-      val changeScore = other.getBetScore(otherScore2 == mix8Score)
-      banker.addScore(this.roomPaijiu, -changeScore)
-      other.addScore(this.roomPaijiu, changeScore)
-      roomPaijiu.addUserSocre(banker.userId, -changeScore)
-      roomPaijiu.addUserSocre(other.userId, changeScore)
-      if(otherScore2 > mix8Score) {//第三道
+      if(bankerScore2 > sky8Score) {//第三道
         banker.addScore(this.roomPaijiu, other.bet.three)
         other.addScore(this.roomPaijiu, -other.bet.three)
         roomPaijiu.addUserSocre(banker.userId, other.bet.three)
         roomPaijiu.addUserSocre(other.userId, -other.bet.three)
+      }
+      other.winState = LOSE
+    } else if (result < 0) {
+      //闲家赢
+      val changeScore = other.getBetScore(otherScore2 >= mix8Score && otherScore2 <= sky8Score)
+      banker.addScore(this.roomPaijiu, -changeScore)
+      other.addScore(this.roomPaijiu, changeScore)
+      roomPaijiu.addUserSocre(banker.userId, -changeScore)
+      roomPaijiu.addUserSocre(other.userId, changeScore)
+      if(otherScore2 > sky8Score) {//第三道
+        banker.addScore(this.roomPaijiu, -other.bet.three)
+        other.addScore(this.roomPaijiu, other.bet.three)
+        roomPaijiu.addUserSocre(banker.userId, -other.bet.three)
+        roomPaijiu.addUserSocre(other.userId, other.bet.three)
       }
       other.winState = WIN
     }
