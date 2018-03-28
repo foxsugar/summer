1d7994bb1b18e90d21369abd909e8ec9ab518d57
foxsugar <redalert3@126.com>
2018-03-28 17:53:46 +0800

    俱乐部

--- login/src/main/scala/com/code/server/login/action/AgentAction.java
+++ login/src/main/scala/com/code/server/login/action/AgentAction.java
@@ -1,20 +1,22 @@
 package com.code.server.login.action;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by sunxianping on 2018/3/14.
  */
 
-@Controller
-@RequestMapping(value = "/")
+@RestController
+@RequestMapping(value = "/agent")
 public class AgentAction {
 
 
@@ -28,4 +30,16 @@ public class AgentAction {
         return "redirect:http://192.168.1.132:8080";
 //        return "/ttt";
     }
+
+
+    @RequestMapping("/getAgentInfo")
+    public AgentResponse getRoomUser(String roomId) {
+        Map<String, Object> data = new HashMap<>();
+        data.put("money", 1);
+        AgentResponse agentResponse = new AgentResponse(0, data);
+
+        return agentResponse;
+
+    }
+
 }
