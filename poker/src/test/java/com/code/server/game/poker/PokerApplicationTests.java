79080521c68769f01436d82e364a059b074f5868
zhaobb <zhaobb@rihuisoft.com>
2018-04-08 09:08:44 +0800

    更新12345

--- poker/src/test/java/com/code/server/game/poker/PokerApplicationTests.java
+++ poker/src/test/java/com/code/server/game/poker/PokerApplicationTests.java
@@ -1,16 +1,49 @@
 package com.code.server.game.poker;
 
+import com.code.server.game.poker.pullmice.CardUtils;
+import com.code.server.game.poker.pullmice.IfCard;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
 public class PokerApplicationTests {
 
-	@Test
-	public void contextLoads() {
+
+	public static void main(String[] args) {
+
+		Integer i = CardUtils.string2Client("大王");
+
+		Integer j = CardUtils.string2Client("小王");
+
+		List<String> list = new ArrayList<>();
+		list.add("♥-A");
+		list.add("♥-5");
+		list.add("♥-3");
+		list.add("♥-4");
+		list.add("♥-2");
+
+		List<Integer> lll = CardUtils.strings2Local(list, new IfCard() {
+			@Override
+			public Map<Integer, Integer> cardDict() {
+				return CardUtils.getCardDict();
+			}
+		});
+
+		List<String> kkkk = CardUtils.localsToString(lll, new IfCard() {
+			@Override
+			public Map<Integer, Integer> cardDict() {
+				return CardUtils.getCardDict();
+			}
+		});
+
+		boolean ret = CardUtils.is12345(lll);
+
+		System.out.println(ret);
 	}
 
 }
