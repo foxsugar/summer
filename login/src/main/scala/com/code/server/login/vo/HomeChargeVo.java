530714bb04de41f39baa8e7e47d2608241ad2ca1
zhaobb <zhaobb@rihuisoft.com>
2018-06-05 09:00:40 +0800

    更新

--- login/src/main/scala/com/code/server/login/vo/HomeChargeVo.java
+++ login/src/main/scala/com/code/server/login/vo/HomeChargeVo.java
@@ -1,5 +1,10 @@
 package com.code.server.login.vo;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by dajuejinxian on 2018/5/29.
  */
@@ -9,6 +14,55 @@ public class HomeChargeVo {
     private String twoLevel;
     private String threeLevel;
     private String total;
+    private String start;
+    private String end;
+
+    @JsonProperty("list1")
+    private List<OneLevelInfoVo> oneLevelVoList = new ArrayList<>();
+    @JsonProperty("list2")
+    private List<TwoLevelInfoVo> twoLevelInfoVoList = new ArrayList<>();
+    @JsonProperty("list3")
+    private List<ThreeLevelInfoVo> threeLevelInfoVoList = new ArrayList<>();
+
+    public List<OneLevelInfoVo> getOneLevelVoList() {
+        return oneLevelVoList;
+    }
+
+    public void setOneLevelVoList(List<OneLevelInfoVo> oneLevelVoList) {
+        this.oneLevelVoList = oneLevelVoList;
+    }
+
+    public List<TwoLevelInfoVo> getTwoLevelInfoVoList() {
+        return twoLevelInfoVoList;
+    }
+
+    public void setTwoLevelInfoVoList(List<TwoLevelInfoVo> twoLevelInfoVoList) {
+        this.twoLevelInfoVoList = twoLevelInfoVoList;
+    }
+
+    public List<ThreeLevelInfoVo> getThreeLevelInfoVoList() {
+        return threeLevelInfoVoList;
+    }
+
+    public void setThreeLevelInfoVoList(List<ThreeLevelInfoVo> threeLevelInfoVoList) {
+        this.threeLevelInfoVoList = threeLevelInfoVoList;
+    }
+
+    public String getStart() {
+        return start;
+    }
+
+    public void setStart(String start) {
+        this.start = start;
+    }
+
+    public String getEnd() {
+        return end;
+    }
+
+    public void setEnd(String end) {
+        this.end = end;
+    }
 
     public String getOnelevel() {
         return onelevel;
