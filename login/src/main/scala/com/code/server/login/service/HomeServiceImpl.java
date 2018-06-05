2db96a9f1697066071775afadf26b77b2bc60f3a
zhaobb <zhaobb@rihuisoft.com>
2018-06-05 14:55:56 +0800

    up del

--- login/src/main/scala/com/code/server/login/service/HomeServiceImpl.java
+++ login/src/main/scala/com/code/server/login/service/HomeServiceImpl.java
@@ -1,9 +1,12 @@
 package com.code.server.login.service;
 
 import com.code.server.constant.game.AgentBean;
+import com.code.server.login.action.AgentAction;
 import com.code.server.login.vo.HomeChargeVo;
 import com.code.server.login.vo.HomePageVo;
 import com.code.server.redis.service.RedisManager;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -16,11 +19,14 @@ public class HomeServiceImpl implements HomeService{
     @Autowired
     private TodayChargeService todayChargeService;
 
+    private static final Logger logger = LoggerFactory.getLogger(HomeServiceImpl.class);
     @Override
     public HomePageVo showHomePage(long agentId) {
         AgentBean agentBean = RedisManager.getAgentRedisService().getAgentBean(agentId);
+
+        logger.info("agentBean{}", agentBean);
         HomePageVo homePageVo = new HomePageVo();
-        homePageVo.setRebate("¥" + agentBean.getRebate());
+        homePageVo.setRebate("" + agentBean.getRebate());
         homePageVo.setInvitationCode("" + agentId);
         HomeChargeVo homeChargeVo = todayChargeService.showCharge(agentId);
         String total = homeChargeVo.getTotal();
