3401760805775b2bbef6cd5f108972a06fac20f7
zhaobb <zhaobb@rihuisoft.com>
2018-06-08 11:47:36 +0800

    up del

---
+++ poker/src/main/scala/com/code/server/game/poker/zhaguzi/YSZRobot.java
@@ -0,0 +1,10 @@
+package com.code.server.game.poker.zhaguzi;
+
+import com.code.server.constant.robot.IRobot;
+
+/**
+ * Created by dajuejinxian on 2018/6/8.
+ */
+public interface YSZRobot extends IRobot {
+    void pass(GameYSZ game);
+}
--- poker/src/main/scala/com/code/server/game/poker/zhaguzi/ZJHRobot.java
+++
@@ -1,8 +0,0 @@
-package com.code.server.game.poker.zhaguzi;
-
-/**
- * Created by dajuejinxian on 2018/6/8.
- */
-public interface ZJHRobot {
-    void open(GameYSZ game);
-}
