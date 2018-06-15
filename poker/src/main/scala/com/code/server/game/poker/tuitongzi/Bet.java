439ef27bf57516e6fc2dece51aac215b51b2a432
zhaobb <zhaobb@rihuisoft.com>
2018-06-15 17:16:20 +0800

    up del

--- poker/src/main/scala/com/code/server/game/poker/tuitongzi/Bet.java
+++ poker/src/main/scala/com/code/server/game/poker/tuitongzi/Bet.java
@@ -8,6 +8,7 @@ public class Bet
     public static final int ER_SHI = 4;
     public static final int GUO_BAN = 5;
     public static final int MAN_ZHU = 6;
+    public static final int STATE_FREE_BET = 1000; //自由下注 从 1000开始 比如 1009 就是 9 注
 
     private int zhu;
     public int getZhu() {
