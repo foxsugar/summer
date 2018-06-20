e6f6a03622c41bafbc7c882bdcd1e1f546100db4
foxsugar <redalert3@126.com>
2018-06-20 11:17:23 +0800

    重构

--- login/src/main/scala/com/code/server/login/action/WechatPayController.java
+++ login/src/main/scala/com/code/server/login/action/WechatPayController.java
@@ -125,6 +125,7 @@ public class WechatPayController {
         String ip = spIp;
         //微信
 
+        money = 1;
         int money100 = money * 100;
 
         Integer moneyPoint = getMoneyPoint(money, chargeType);
