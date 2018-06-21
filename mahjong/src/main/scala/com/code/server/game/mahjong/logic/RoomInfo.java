de37036a45d2832ac8f102561167e3d5100c2de8
foxsugar <redalert3@126.com>
2018-06-21 12:47:53 +0800

    重构

--- mahjong/src/main/scala/com/code/server/game/mahjong/logic/RoomInfo.java
+++ mahjong/src/main/scala/com/code/server/game/mahjong/logic/RoomInfo.java
@@ -401,7 +401,8 @@ public class RoomInfo extends RoomExtendGold {
         result.put("haveTing", this.haveTing);
         result.put("clubId", this.getClubId());
         result.put("clubRoomModel", this.getClubRoomModel());
-
+        result.put("goldRoomType", this.getGoldRoomType());
+        result.put("goldRoomPermission", this.getGoldRoomPermission());
 
         return result;
     }
@@ -474,6 +475,7 @@ public class RoomInfo extends RoomExtendGold {
     public PrepareRoom getPrepareRoomVo() {
         PrepareRoomMj prepareRoom = new PrepareRoomMj();
         prepareRoom.goldRoomType = this.goldRoomType;
+        prepareRoom.goldRoomPermission = this.goldRoomPermission;
         prepareRoom.createTime = System.currentTimeMillis();
         prepareRoom.personNumber = this.personNumber;
         prepareRoom.gameType = this.getGameType();
