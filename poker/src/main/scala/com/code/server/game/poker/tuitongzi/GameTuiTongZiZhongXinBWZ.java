8b3da5cd60d2cc730e2da054a61838f1283921b5
zhaobb <zhaobb@rihuisoft.com>
2018-06-28 15:47:30 +0800

    up del

--- poker/src/main/scala/com/code/server/game/poker/tuitongzi/GameTuiTongZiZhongXinBWZ.java
+++ poker/src/main/scala/com/code/server/game/poker/tuitongzi/GameTuiTongZiZhongXinBWZ.java
@@ -4,6 +4,10 @@ package com.code.server.game.poker.tuitongzi;
  */
 public class GameTuiTongZiZhongXinBWZ extends  GameTuiTongZiZhongXin{
 
+    //偏移量， 上来要在锅里放 多少钱
+    protected long offset(){
+        return 60;
+    };
     /*一下是霸王庄条件 */
     protected boolean isBaWangZhuang(){
         return true;
