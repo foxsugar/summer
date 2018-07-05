ec1393ab0b8ece47fc1db274bd15a3cd5111ae3a
zhaobb <zhaobb@rihuisoft.com>
2018-07-05 13:41:17 +0800

    up del

--- db/src/main/java/com/code/server/db/dao/IChargeDao.java
+++ db/src/main/java/com/code/server/db/dao/IChargeDao.java
@@ -15,11 +15,11 @@ import java.util.List;
 public interface IChargeDao extends PagingAndSortingRepository<Charge, Long>, JpaSpecificationExecutor<Charge> {
 
     Charge getChargeByOrderId(String orderId);
-
-    @Query(value = "select '*' from charge where charge.userid IN ?1 AND createtime BETWEEN ?2 AND ?3 AND status = ?4 AND charge_type IN ?5", nativeQuery = true)
+//    @Query(value = "select '*' from charge where charge.userid IN ?1 AND createtime BETWEEN ?2 AND ?3 AND status = ?4 AND charge_type IN ?5", nativeQuery = true)
+    @Query(value = "select '*' from charge where charge.userid IN ?1 AND createtime BETWEEN ?2 AND ?3  AND charge_type IN ?5", nativeQuery = true)
     List<Charge> getChargesByUseridInAndCreatetimeBetweenAndStatusIsAndChargeTypeIn(List<Long> users, Date start, Date end, int status, List<Integer> list);
-
-    @Query(value = "select '*' from charge where charge.userid IN ?1 AND createtime BETWEEN ?2 AND ?3 AND status = ?4 AND recharge_source = ?5", nativeQuery = true)
+//    @Query(value = "select '*' from charge where charge.userid IN ?1 AND createtime BETWEEN ?2 AND ?3 AND status = ?4 AND recharge_source = ?5", nativeQuery = true)
+    @Query(value = "select '*' from charge where charge.userid IN ?1 AND createtime BETWEEN ?2 AND ?3  AND recharge_source = ?5", nativeQuery = true)
     List<Charge> getChargesByUseridInAndCreatetimeBetweenAndStatusIsAndRecharge_sourceIs(List<Long> users, Date start, Date end, int status, String sourceType);
 
     @Query(value = "select '*' from charge where charge_type IN ?1", nativeQuery = true)
@@ -31,6 +31,4 @@ public interface IChargeDao extends PagingAndSortingRepository<Charge, Long>, Jp
     @Query(value = "select sum(money) from charge where charge.userid IN ?1 AND createtime BETWEEN ?2 AND ?3", nativeQuery = true)
     Integer getSumMoneyByUsersAndDate(List<Long> ids,Date start, Date end);
 
-    @Query(value = "select '*' from charge where charge.userid = ?1 and recharge_source = ?2 AND createtime BETWEEN ?3 AND ?4 AND status =1", nativeQuery = true)
-    List<Charge> getChargesByUseridAndRechargeSourceAndDate(Long id, String chargeTye, Date start, Date end);
 }
