a32066ce16ea2b201896f8c5902871eddac29447
clarkkent <lykaka22@126.com>
2018-08-22 15:56:23 +0800

    fix seven

---
+++ poker/src/main/scala/com/code/server/game/poker/cow/Demo.java
@@ -0,0 +1,36 @@
+package com.code.server.game.poker.cow;
+
+/**
+ * 项目名称：${project_name}
+ * 类名称：${type_name}
+ * 类描述：
+ * 创建人：Clark
+ * 创建时间：${date} ${time}
+ * 修改人：Clark
+ * 修改时间：${date} ${time}
+ * 修改备注：
+ *
+ * @version 1.0
+ */
+public class Demo {
+    public static void main(String[] args) {
+        System.out.println(Integer.parseInt(Math.scalb(1.0,1)+""));
+        System.out.println(Math.scalb(1.0,1));
+        System.out.println(Math.scalb(1.0,2));
+        System.out.println(Math.scalb(1.0,3));
+        System.out.println(Math.scalb(1.0,4));
+        /*System.out.println(2%3);
+        System.out.println(3%3);
+        while (true){
+            System.out.println((int)System.currentTimeMillis()%2);
+        }*/
+        /*Map<Integer,Integer> map = new HashMap<>();
+        List<Integer> list = new ArrayList<>();
+        list.add(0,12);
+        list.add(1,12);
+        list.add(12);
+        list.add(null);
+        list.add(null);
+        System.out.println(list);*/
+    }
+}
