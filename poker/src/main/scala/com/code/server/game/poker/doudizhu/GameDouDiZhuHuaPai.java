2fcf0b4a99c7d0786be35faf3423b47074a8c726
foxsugar <redalert3@126.com>
2018-08-29 17:19:58 +0800

    重构

--- poker/src/main/scala/com/code/server/game/poker/doudizhu/GameDouDiZhuHuaPai.java
+++ poker/src/main/scala/com/code/server/game/poker/doudizhu/GameDouDiZhuHuaPai.java
@@ -6,13 +6,26 @@ import java.util.List;
 /**
  * Created by sunxianping on 2018/8/29.
  */
-public class GameDouDiHuaPai extends GameDouDiZhuLinFenLongQi {
+public class GameDouDiZhuHuaPai extends GameDouDiZhuLinFenLongQi {
 
 
     @Override
     public void init(List<Long> users, long dizhuUser) {
         this.initCardNum = 17;
-        super.init(users, dizhuUser);
+        //初始化玩家
+        for (Long uid : users) {
+            PlayerCardInfoDouDiZhu playerCardInfo = getGameTypePlayerCardInfo();
+            playerCardInfo.userId = uid;
+            playerCardInfos.put(uid, playerCardInfo);
+        }
+        this.users.addAll(users);
+
+
+        shuffle();
+        deal();
+        //第一局 第一个玩家做地主
+        dizhuUser = dizhuUser != 0 ? dizhuUser : users.get(0);
+        chooseDizhu(dizhuUser);
 
         //生成操作列表
         operateNode = OperateNode.initOperate(canJiaoUser, users);
