edee85ae9ca2f4baaabd76ff6401084ef8e731c5
foxsugar <redalert3@126.com>
2019-04-24 16:43:22 +0800

    重构

--- login/src/main/scala/com/code/server/login/action/LoginAction.java
+++ login/src/main/scala/com/code/server/login/action/LoginAction.java
@@ -438,6 +438,7 @@ public class LoginAction {
         newUser.setVip(0);
         newUser.setUuid("0");
         newUser.setMoney(getConstant().getInitMoney());
+        newUser.setGold(getConstant().getInitGold());
         newUser.setRegistDate(new Date());
         newUser.setLastLoginDate(new Date());
 
