23864ff6a7ba7f51f899714c9b4e5babe8897211
foxsugar <redalert3@126.com>
2019-05-23 17:59:34 +0800

    重构

--- poker/src/main/scala/com/code/server/game/poker/paijiu/PaijiuRobot.scala
+++ poker/src/main/scala/com/code/server/game/poker/paijiu/PaijiuRobot.scala
@@ -130,13 +130,13 @@ class PaijiuRobot extends IRobot with PaijiuConstant {
           rp.updateBanker()
           //选定庄家后10秒开局
           if ((now - rp.getLastOperateTime) > STATE_TIME(STATE_START) && rp.getBankerId != 0) {
-            println("托管: 开始游戏")
+            println("托管: 开始游戏 100 " + room.getRoomId)
             sendStartGame(rp)
           }
         }else{
           val rpc = room.asInstanceOf[RoomPaijiuCrazy]
           if ((now - rpc.getLastOperateTime) > STATE_TIME(STATE_START) && rpc.getCurGameNumber>1) {
-            println("托管: 开始游戏")
+            println("托管: 开始游戏 四人 " + room.getRoomId)
             sendStartGame(rpc)
           }
         }
