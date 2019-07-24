f551ede2e804eae01e0d852cd75565a24be3f468
foxsugar <redalert3@126.com>
2019-07-24 16:43:08 +0800

    data

--- login/src/main/scala/com/code/server/login/action/TestAction.java
+++ login/src/main/scala/com/code/server/login/action/TestAction.java
@@ -7,9 +7,12 @@ import com.code.server.db.Service.UserService;
 import com.code.server.db.model.Charge;
 import com.code.server.db.model.User;
 import com.code.server.login.anotation.AuthChecker;
+import com.code.server.login.config.ServerConfig;
 import com.code.server.login.kafka.MsgSender;
 import com.code.server.login.util.PayUtil;
 import com.code.server.redis.service.UserRedisService;
+import com.code.server.util.DateUtil;
+import com.code.server.util.SpringUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -24,6 +27,7 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -43,18 +47,18 @@ public class TestAction {
     @Autowired
     private UserService userService;
 
-    protected static Logger logger= LoggerFactory.getLogger(TestAction.class);
+    protected static Logger logger = LoggerFactory.getLogger(TestAction.class);
 
-    private static final String keyValue = "xhzw2malfjk62p0g8m9by7ycx97fqahv" ;
+    private static final String keyValue = "?9kehci9n1kn3q17lhm9d64itq3i7q8vn";
     private UserBean userBeanRedis;
 
     @RequestMapping("/index")
-    public String index(){
+    public String index() {
         return "index";
     }
 
     @RequestMapping("/pay")
-    public void pay(@RequestParam(value = "money", required = true) Double money,  @RequestParam(value = "uid", required = true) Long uid, HttpServletRequest request, HttpServletResponse resp) throws ServletException, IOException {
+    public void pay(@RequestParam(value = "money", required = true) Double money, @RequestParam(value = "uid", required = true) Long uid, HttpServletRequest request, HttpServletResponse resp) throws ServletException, IOException {
 
         String orderId = PayUtil.getOrderIdByUUId();
         Charge charge = new Charge();
@@ -72,36 +76,41 @@ public class TestAction {
 
 
     @RequestMapping("/pay1")
-    public void pay1(@RequestParam(value = "money", required = true) Double money,  @RequestParam(value = "uid", required = true) Long uid, HttpServletRequest request, HttpServletResponse resp) throws ServletException, IOException {
+    public void pay1(@RequestParam(value = "money", required = true) Double money, @RequestParam(value = "uid", required = true) Long uid, String platform,
+                     HttpServletRequest request, HttpServletResponse resp) throws ServletException, IOException {
 
         String orderId = PayUtil.getOrderIdByUUId();
         Charge charge = new Charge();
         charge.setOrderId(orderId);
         charge.setUserid(uid);
         charge.setMoney(money);
-        charge.setMoney_point(money * 10);
+        charge.setMoney_point(money);
         charge.setStatus(0);
+        charge.setCreatetime(new Date());
+        charge.setChargeType(0);
+        charge.setOrigin(0);
+        charge.setRecharge_source(platform);
         chargeService.save(charge);
         logger.info(charge.getOrderId());
         request.setAttribute("Moneys", money + "");
-        request.setAttribute("Bankco", "zfb");
+        request.setAttribute("Bankco", platform);
         request.setAttribute("orderId", orderId);
         request.getRequestDispatcher("/WEB-INF/jsp/pay1.jsp").forward(request, resp);
     }
 
     @RequestMapping("/Pay/notify")
-    public String noti(HttpServletRequest request){
-        String memberid=request.getParameter("memberid");
-        String orderid=request.getParameter("orderid");
-        String amount=request.getParameter("amount");
-        String datetime=request.getParameter("datetime");
-        String returncode=request.getParameter("returncode");
-        String reserved1=request.getParameter("reserved1");
-        String reserved2=request.getParameter("reserved2");
-        String sign=request.getParameter("sign");
+    public String noti(HttpServletRequest request) {
+        String memberid = request.getParameter("memberid");
+        String orderid = request.getParameter("orderid");
+        String amount = request.getParameter("amount");
+        String datetime = request.getParameter("datetime");
+        String returncode = request.getParameter("returncode");
+        String reserved1 = request.getParameter("reserved1");
+        String reserved2 = request.getParameter("reserved2");
+        String sign = request.getParameter("sign");
         String transaction_id = request.getParameter("transaction_id");
-        String SignTemp="amount="+amount+"&datetime="+datetime + "&memberid="+memberid+"&orderid="+orderid+"&transaction_id="+transaction_id+"&returncode="+returncode+"&key=" + keyValue + "";
-        String md5sign= null;
+        String SignTemp = "amount=" + amount + "&datetime=" + datetime + "&memberid=" + memberid + "&orderid=" + orderid + "&transaction_id=" + transaction_id + "&returncode=" + returncode + "&key=" + keyValue + "";
+        String md5sign = null;
         try {
             md5sign = md5(SignTemp);
         } catch (NoSuchAlgorithmException e) {
@@ -109,16 +118,17 @@ public class TestAction {
         }
 
         logger.info("-------------------------------");
-        logger.info("memberid{}, orderid{}, amount{}, datetime{}, requestcode{}, returncode{}, reserved1{}, reserverd2{}, sign{}, tempSign{}",memberid,orderid, amount, datetime, returncode, sign, SignTemp);
+        logger.info("memberid{}, orderid{}, amount{}, datetime{}, requestcode{}, returncode{}, reserved1{}, reserverd2{}, sign{}, tempSign{}", memberid, orderid, amount, datetime, returncode, sign, SignTemp);
         logger.info("-------------------------------");
 
         logger.info("local sign：{}", md5sign);
         logger.info("sign：{}", sign);
 
-        if (true){
-            if(returncode.equals("00")){
+        if (true) {
+            if (returncode.equals("00")) {
+                ServerConfig serverConfig = SpringUtil.getBean(ServerConfig.class);
                 Charge charge = chargeService.getChargeByOrderid(orderid);
-                if (charge.getStatus() == 1){
+                if (charge.getStatus() == 1) {
                     return "success";
                 }
 
@@ -126,26 +136,35 @@ public class TestAction {
                 chargeService.save(charge);
                 logger.info("支付成功！");
 
+                charge.setCallbacktime(new Date());
+                int money = (int) charge.getMoney();
+                String dayStr = DateUtil.convert2DayString(new Date());
+                //更新订单结算是否已经返利
+                charge.setFinishTime(dayStr);
+
+                int addMoney = serverConfig.getChargeMap().get(money);
                 UserBean UserBeanRedis = userRedisService.getUserBean(charge.getUserid());
-                double addMoney = charge.getMoney() * 10;
 
                 if (UserBeanRedis != null) {
                     userRedisService.addUserMoney(charge.getUserid(), addMoney);
+                    charge.setUsername(UserBeanRedis.getUsername());
                 } else {
                     User user = userService.getUserByUserId(charge.getUserid());
-                    System.out.println("修改玩家豆豆");
                     user.setMoney(user.getMoney() + addMoney);
+                    charge.setUsername(user.getUsername());
                     userService.save(user);
                 }
 
                 System.out.println("通知客户端刷新充值");
                 Map<String, String> rs = new HashMap<>();
                 MsgSender.sendMsg2Player(new ResponseVo("userService", "refresh", rs), charge.getUserid());
-            }else{
+
+
+            } else {
                 logger.info("支付失败");
                 return "failed";
             }
-        }else{
+        } else {
             logger.info("支付失败");
             return "failed";
         }
@@ -154,21 +173,21 @@ public class TestAction {
     }
 
     @RequestMapping("/Pay/callback")
-    public String callback(HttpServletRequest request){
+    public String callback(HttpServletRequest request) {
 
-        String memberid=request.getParameter("memberid");
-        String orderid=request.getParameter("orderid");
-        String amount=request.getParameter("amount");
-        String datetime=request.getParameter("datetime");
-        String returncode=request.getParameter("returncode");
-        String sign=request.getParameter("sign");
+        String memberid = request.getParameter("memberid");
+        String orderid = request.getParameter("orderid");
+        String amount = request.getParameter("amount");
+        String datetime = request.getParameter("datetime");
+        String returncode = request.getParameter("returncode");
+        String sign = request.getParameter("sign");
 
         String transaction_id = request.getParameter("transaction_id");
         logger.info("{}", transaction_id);
-        String SignTemp="amount="+amount+"&datetime="+datetime + "&memberid="+memberid+"&orderid="+orderid+"&transaction_id="+transaction_id+"&returncode="+returncode+"&key=" + keyValue + "";
+        String SignTemp = "amount=" + amount + "&datetime=" + datetime + "&memberid=" + memberid + "&orderid=" + orderid + "&transaction_id=" + transaction_id + "&returncode=" + returncode + "&key=" + keyValue + "";
 
 
-        String md5sign= null;
+        String md5sign = null;
         try {
             md5sign = md5(SignTemp);
         } catch (NoSuchAlgorithmException e) {
@@ -176,41 +195,18 @@ public class TestAction {
         }
 
         logger.info("-------------------------------");
-        logger.info("memberid{}, orderid{}, amount{}, datetime{}, requestcode{}, returncode{}, reserved1{}, reserverd2{}, sign{}, tempSign{}",memberid,orderid, amount, datetime, returncode, sign, SignTemp);
+        logger.info("memberid{}, orderid{}, amount{}, datetime{}, requestcode{}, returncode{}, reserved1{}, reserverd2{}, sign{}, tempSign{}", memberid, orderid, amount, datetime, returncode, sign, SignTemp);
         logger.info("-------------------------------");
 
         logger.info("-local sign：{}", md5sign);
         logger.info("-sign：{}", sign);
-        if (true){
-            if(returncode.equals("00")){
-                Charge charge = chargeService.getChargeByOrderid(orderid);
-                if (charge.getStatus() == 1){
-                    return "success";
-                }
-                charge.setStatus(1);
-                chargeService.save(charge);
-                logger.info("支付成功！");
-
-                UserBean UserBeanRedis = userRedisService.getUserBean(charge.getUserid());
-                double addMoney = charge.getMoney();
-
-                if (UserBeanRedis != null) {
-                    userRedisService.addUserMoney(charge.getUserid(), addMoney);
-                } else {
-                    User user = userService.getUserByUserId(charge.getUserid());
-                    System.out.println("修改玩家豆豆");
-                    user.setMoney(user.getMoney() + addMoney);
-                    userService.save(user);
-                }
-
-                System.out.println("通知客户端刷新充值");
-                Map<String, String> rs = new HashMap<>();
-                MsgSender.sendMsg2Player(new ResponseVo("userService", "refresh", rs), charge.getUserid());
-            }else{
+        if (true) {
+            if (returncode.equals("00")) {
+            } else {
                 logger.info("支付失败");
                 return "failed";
             }
-        }else{
+        } else {
             logger.info("支付失败");
             return "failed";
         }
@@ -221,7 +217,7 @@ public class TestAction {
     @ResponseBody
     @RequestMapping("/test")
     @AuthChecker
-    public String test(){
+    public String test() {
         return "hello world";
     }
 
--- login/src/main/webapp/WEB-INF/jsp/pay1.jsp
+++ login/src/main/webapp/WEB-INF/jsp/pay1.jsp
@@ -27,19 +27,19 @@ margin-top:150px;text-align:center;display:block; border:3px solid #d9d9de; padd
     String Channelid=request.getParameter("Bankco");
     String  Moneys    = request.getParameter("Moneys");  //金额
     String     pay_bankcode=null;
-    if(Channelid.equals("wx")){
+    if(Channelid.equals("1")){
          pay_bankcode="902";   //'银行编码
-    }else if(Channelid.equals("zfb")){
+    }else if(Channelid.equals("2")){
          pay_bankcode="903";   //'银行编码
     }
         String    pay_memberid=merchantId;//商户id
-        String    pay_orderid=generateOrderId();//20位订单号 时间戳+6位随机字符串组成
+        String    pay_orderid= request.getParameter("orderId");//20位订单号 时间戳+6位随机字符串组成
         String    pay_applydate=generateTime();//yyyy-MM-dd HH:mm:ss
         String    pay_notifyurl="http://"+request.getRequestURI()+"/Pay/notifyurl.asp";//通知地址
         String    pay_callbackurl="http://"+request.getRequestURI()+"/Pay/Callback.asp";//回调地址
         String    pay_amount=Moneys;
         String    pay_attach="";
-        String    pay_productname="100元话费充值";
+        String    pay_productname="充值";
         String    pay_productnum="";
         String    pay_productdesc="";
         String    pay_producturl="";    
@@ -58,9 +58,6 @@ margin-top:150px;text-align:center;display:block; border:3px solid #d9d9de; padd
 <input type="hidden" name="pay_notifyurl"  value="<%=pay_notifyurl%>">
 <input type="hidden" name="pay_callbackurl"  value="<%=pay_callbackurl%>">
 <input type="hidden" name="pay_amount"  value="<%=pay_amount%>">
-<input type="hidden" name="pay_reserved1"  value="<%=pay_reserved1%>">
-<input type="hidden" name="pay_reserved2"  value="<%=pay_reserved2%>">
-<input type="hidden" name="pay_reserved3"  value="<%=pay_reserved3%>">
 <input type="hidden" name="pay_productname"  value="<%=pay_productname%>">
 <input type="hidden" name="pay_productnum"  value="<%=pay_productnum%>">
 <input type="hidden" name="pay_productdesc"  value="<%=pay_productdesc%>">
