2c2a2586aa31366828a3a5a6bac184ced95a95e1
foxsugar <redalert3@126.com>
2019-09-02 10:53:55 +0800

    data

--- login/src/main/scala/com/code/server/login/action/ManagerAction.java
+++ login/src/main/scala/com/code/server/login/action/ManagerAction.java
@@ -293,19 +293,19 @@ public class ManagerAction extends Cors {
         return 0;
     }
 
-    @RequestMapping("/clearRebate")
-    public Object clearRebate() {
-        for(String userId : RedisManager.getUserRedisService().getAllUserId()){
-            long uid = Long.valueOf(userId);
-            RedisManager.getUserRedisService().setUserGold(uid,0);
-            UserBean userBean = RedisManager.getUserRedisService().getUserBean(uid);
-            userBean.setGold(0);
-            userBean.getUserInfo().setAllRebate(0);
-            RedisManager.getUserRedisService().updateUserBean(uid, userBean);
-
-        }
-        return 0;
-    }
+//    @RequestMapping("/clearRebate")
+//    public Object clearRebate() {
+//        for(String userId : RedisManager.getUserRedisService().getAllUserId()){
+//            long uid = Long.valueOf(userId);
+//            RedisManager.getUserRedisService().setUserGold(uid,0);
+//            UserBean userBean = RedisManager.getUserRedisService().getUserBean(uid);
+//            userBean.setGold(0);
+//            userBean.getUserInfo().setAllRebate(0);
+//            RedisManager.getUserRedisService().updateUserBean(uid, userBean);
+//
+//        }
+//        return 0;
+//    }
 
     public static void main(String[] args) {
         LocalDate localDate = LocalDate.now();
