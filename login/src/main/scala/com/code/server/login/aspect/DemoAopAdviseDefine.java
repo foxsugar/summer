133c4c98e417c16f282fa13cb0fbee7461c45955
sunxp <sunxp@123.com>
2020-04-16 16:26:00 +0800

    seat

--- login/src/main/scala/com/code/server/login/aspect/DemoAopAdviseDefine.java
+++ login/src/main/scala/com/code/server/login/aspect/DemoAopAdviseDefine.java
@@ -1,43 +1,43 @@
-package com.code.server.login.aspect;
-
-import com.code.server.login.action.AgentResponse;
-import com.code.server.login.util.AgentUtil;
-import com.code.server.redis.service.RedisManager;
-import org.aspectj.lang.ProceedingJoinPoint;
-import org.aspectj.lang.annotation.Around;
-import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Pointcut;
-import org.springframework.stereotype.Component;
-import org.springframework.web.context.request.RequestAttributes;
-import org.springframework.web.context.request.RequestContextHolder;
-import org.springframework.web.context.request.ServletRequestAttributes;
-import sun.management.Agent;
-
-import javax.servlet.http.Cookie;
-import javax.servlet.http.HttpServletRequest;
-import java.util.Enumeration;
-import java.util.HashMap;
-import java.util.Map;
-
-/**
- * Created by dajuejinxian on 2018/7/9.
- */
-@Component
-@Aspect
-public class DemoAopAdviseDefine {
-
-    @Pointcut("@annotation(com.code.server.login.anotation.DemoChecker)")
-    public void pointcut(){
-    }
-
-    @Around("pointcut()")
-    public Object checkAuth(ProceedingJoinPoint joinPoint) throws Throwable {
-        String token = AgentUtil.findTokenInHeader();
-        if (!AgentUtil.caches.keySet().contains(token)){
-            Map<String, Object> rs = new HashMap<>();
-            return new AgentResponse(50008, rs);
-        }
-        return joinPoint.proceed();
-    }
-
-}
+//package com.code.server.login.aspect;
+//
+//import com.code.server.login.action.AgentResponse;
+//import com.code.server.login.util.AgentUtil;
+//import com.code.server.redis.service.RedisManager;
+//import org.aspectj.lang.ProceedingJoinPoint;
+//import org.aspectj.lang.annotation.Around;
+//import org.aspectj.lang.annotation.Aspect;
+//import org.aspectj.lang.annotation.Pointcut;
+//import org.springframework.stereotype.Component;
+//import org.springframework.web.context.request.RequestAttributes;
+//import org.springframework.web.context.request.RequestContextHolder;
+//import org.springframework.web.context.request.ServletRequestAttributes;
+//import sun.management.Agent;
+//
+//import javax.servlet.http.Cookie;
+//import javax.servlet.http.HttpServletRequest;
+//import java.util.Enumeration;
+//import java.util.HashMap;
+//import java.util.Map;
+//
+///**
+// * Created by dajuejinxian on 2018/7/9.
+// */
+//@Component
+//@Aspect
+//public class DemoAopAdviseDefine {
+//
+//    @Pointcut("@annotation(com.code.server.login.anotation.DemoChecker)")
+//    public void pointcut(){
+//    }
+//
+//    @Around("pointcut()")
+//    public Object checkAuth(ProceedingJoinPoint joinPoint) throws Throwable {
+//        String token = AgentUtil.findTokenInHeader();
+//        if (!AgentUtil.caches.keySet().contains(token)){
+//            Map<String, Object> rs = new HashMap<>();
+//            return new AgentResponse(50008, rs);
+//        }
+//        return joinPoint.proceed();
+//    }
+//
+//}
--- login/src/main/scala/com/code/server/login/aspect/HttpAopAdviseDefine.java
+++ login/src/main/scala/com/code/server/login/aspect/HttpAopAdviseDefine.java
@@ -1,76 +1,76 @@
-package com.code.server.login.aspect;
-
-import com.code.server.login.action.AgentAction;
-import com.code.server.login.action.AgentResponse;
-import com.code.server.redis.service.RedisManager;
-import org.aspectj.lang.ProceedingJoinPoint;
-import org.aspectj.lang.annotation.Around;
-import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Pointcut;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.redis.core.StringRedisTemplate;
-import org.springframework.stereotype.Component;
-import org.springframework.web.context.request.RequestContextHolder;
-import org.springframework.web.context.request.ServletRequestAttributes;
-
-import javax.servlet.http.Cookie;
-import javax.servlet.http.HttpServletRequest;
-import java.util.HashMap;
-import java.util.Map;
-
-/**
- * Created by dajuejinxian on 2018/6/1.
- */
-@Component
-@Aspect
-public class HttpAopAdviseDefine {
-
-    private static final String AGENT_COOKIE_NAME = "AGENT_TOKEN";
-
-    private static final Logger logger = LoggerFactory.getLogger(HttpAopAdviseDefine.class);
-    @Autowired
-    private StringRedisTemplate redisTemplate;
-
-    @Pointcut("@annotation(com.code.server.login.anotation.AuthChecker)")
-    public void pointcut(){
-    }
-
-    @Around("pointcut()")
-    public Object checkAuth(ProceedingJoinPoint joinPoint) throws Throwable {
-        HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes())
-                .getRequest();
-
-        // 检查用户所传递的 token 是否合法
-        Map<String, String> map = getAgentByToken(request);
-
-        Object[] objects = joinPoint.getArgs();
-
-        if (null == map) {
-//            return "错误, 请登录!";
-            Map<String, Object> rs = new HashMap<>();
-            return new AgentResponse(1000, rs);
-        };
-
-        logger.info("----------map is -- {}", map);
-        return joinPoint.proceed();
-    }
-
-    public static Map<String, String> getAgentByToken(HttpServletRequest request) {
-        Cookie[] cookies = request.getCookies();
-        if (cookies == null) return null;
-        Cookie cookie = null;
-        for (Cookie c : cookies) {
-            if (AGENT_COOKIE_NAME.equals(c.getName())) {
-                cookie = c;
-            }
-        }
-        if (cookie != null) {
-            return RedisManager.getAgentRedisService().getAgentByToken(cookie.getValue());
-        }
-
-        return null;
-    }
-
-}
+//package com.code.server.login.aspect;
+//
+//import com.code.server.login.action.AgentAction;
+//import com.code.server.login.action.AgentResponse;
+//import com.code.server.redis.service.RedisManager;
+//import org.aspectj.lang.ProceedingJoinPoint;
+//import org.aspectj.lang.annotation.Around;
+//import org.aspectj.lang.annotation.Aspect;
+//import org.aspectj.lang.annotation.Pointcut;
+//import org.slf4j.Logger;
+//import org.slf4j.LoggerFactory;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.data.redis.core.StringRedisTemplate;
+//import org.springframework.stereotype.Component;
+//import org.springframework.web.context.request.RequestContextHolder;
+//import org.springframework.web.context.request.ServletRequestAttributes;
+//
+//import javax.servlet.http.Cookie;
+//import javax.servlet.http.HttpServletRequest;
+//import java.util.HashMap;
+//import java.util.Map;
+//
+///**
+// * Created by dajuejinxian on 2018/6/1.
+// */
+//@Component
+//@Aspect
+//public class HttpAopAdviseDefine {
+//
+//    private static final String AGENT_COOKIE_NAME = "AGENT_TOKEN";
+//
+//    private static final Logger logger = LoggerFactory.getLogger(HttpAopAdviseDefine.class);
+//    @Autowired
+//    private StringRedisTemplate redisTemplate;
+//
+//    @Pointcut("@annotation(com.code.server.login.anotation.AuthChecker)")
+//    public void pointcut(){
+//    }
+//
+//    @Around("pointcut()")
+//    public Object checkAuth(ProceedingJoinPoint joinPoint) throws Throwable {
+//        HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes())
+//                .getRequest();
+//
+//        // 检查用户所传递的 token 是否合法
+//        Map<String, String> map = getAgentByToken(request);
+//
+//        Object[] objects = joinPoint.getArgs();
+//
+//        if (null == map) {
+////            return "错误, 请登录!";
+//            Map<String, Object> rs = new HashMap<>();
+//            return new AgentResponse(1000, rs);
+//        };
+//
+//        logger.info("----------map is -- {}", map);
+//        return joinPoint.proceed();
+//    }
+//
+//    public static Map<String, String> getAgentByToken(HttpServletRequest request) {
+//        Cookie[] cookies = request.getCookies();
+//        if (cookies == null) return null;
+//        Cookie cookie = null;
+//        for (Cookie c : cookies) {
+//            if (AGENT_COOKIE_NAME.equals(c.getName())) {
+//                cookie = c;
+//            }
+//        }
+//        if (cookie != null) {
+//            return RedisManager.getAgentRedisService().getAgentByToken(cookie.getValue());
+//        }
+//
+//        return null;
+//    }
+//
+//}
